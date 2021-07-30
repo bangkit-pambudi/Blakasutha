fc53aa1330a0c7a9abc6c01925ec3fb417ab14d1
Andreas Sahir Aryanto <70127732+algebraicsum@users.noreply.github.com>
2021-07-30 14:45:05 +0700

    Update Lengan_Putar.ino

--- Main/Lengan_Putar.ino
+++ Main/Lengan_Putar.ino
@@ -1,9 +1,9 @@
 void LenganPutar(){
-if (ps2x.Button(PSB_L1) && ps2x.Button(PSB_SQUARE)) { //L1 + Kotak
+if (ps2x.Button(PSB_L2) && ps2x.Button(PSB_SQUARE)) { //L2 + Kotak
     Serial.println("Motor memutar ke belakang");
     analogWrite(roller_pwm1, 60);
     analogWrite(roller_pwm2, 0);
-  }else if (ps2x.Button(PSB_L1) && ps2x.Button(PSB_TRIANGLE)){ //L1 + Segitiga
+  }else if (ps2x.Button(PSB_L2) && ps2x.Button(PSB_TRIANGLE)){ //L2 + Segitiga
     Serial.println("Motor memutar ke depan");
     analogWrite(roller_pwm1, 0);
     analogWrite(roller_pwm2, 60);  
