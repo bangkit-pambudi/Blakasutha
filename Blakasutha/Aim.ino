657784f5005611b9a169805c640924ea784f7ded
algebraicsum <andreas.aryanto@mhs,unsoed.ac.id>
2021-09-10 13:01:57 +0700

    regresi pot 1 dan 2

--- Blakasutha/Aim.ino
+++ Blakasutha/Aim.ino
@@ -43,8 +43,18 @@ void Aim(){ //testing aim robot
   }
   if(ps2x.ButtonPressed(PSB_R2))
   {
-    X = 0;
-    Delay = regress_4(pressureValue);
+    if(ps2x.ButtonPressed(PSB_PAD_UP)){
+        X = 0;
+        Delay = regress_4(pressureValue);
+    }
+    if(ps2x.ButtonPressed(PSB_PAD_RIGHT)){
+        X = 0;
+        Delay = regress_2(pressureValue);
+    }
+    if(ps2x.ButtonPressed(PSB_PAD_DOWN)){
+        X = 0;
+        Delay = regress_1(pressureValue);
+    }
   }
 
   Serial.println(Delay);
@@ -60,6 +70,42 @@ double regress_4(double x) {
     -4.5696514909697780e+003,
      1.8136746675770752e+002,
     -1.7360965295169812e+000
+};
+  
+  double t = 1;
+  double r = 0;
+  for (double c : terms) {
+    r += c * t;
+    t *= x;
+  }
+  return r;
+}
+
+double regress_2(double x) {
+  double terms[] = {
+    -1.1461195242370218e+007,
+     1.0779281696440135e+006,
+    -3.7944196867966908e+004,
+     5.9255845541379961e+002,
+    -3.4641717499497831e+000
+};
+  
+  double t = 1;
+  double r = 0;
+  for (double c : terms) {
+    r += c * t;
+    t *= x;
+  }
+  return r;
+}
+
+double regress_1(double x) {
+  double terms[] = {
+    -4.7894934899005498e+005,
+     5.3961466330134543e+004,
+    -2.2720411681854780e+003,
+     4.2388752175912948e+001,
+    -2.9571727552298488e-001
 };
   
   double t = 1;
